4a595788b8bc4ee4fbd682097c1a84fcfd48be83
jarmibe7 <jarmibe7@gmail.com>
2025-04-11 14:23:11 -0500

    Week 2 Milestone 1 (filtering)

--- plot.py
+++ plot.py
@@ -3,12 +3,22 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 def main():
-    filename = 'data.csv'
+    axis = 'pitch'
+    filename = f'{axis}_data.csv'
     df = pd.read_csv(filename)
+    accel = np.array(df.iloc[:,3])
+    intl = np.array(df.iloc[:,4])
+    filter = np.array(df.iloc[:,5])
+    t = np.linspace(0, len(accel)-1, len(accel))
+
+    
 
     plt.figure(figsize=(10, 5))
-    plt.plot(df['x'], df['y'], marker='o', linestyle='-', color='b')
-
+    plt.plot(t, accel, color='blue', label=f'accel_{axis}')
+    plt.plot(t, intl, color='red', label=f'intl_{axis}')
+    plt.plot(t, filter, color='yellow', label=f'filter_{axis}')
+    plt.legend()
+    plt.savefig(f'{axis}_plot.png')
 
     return
 
