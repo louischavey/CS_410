d5dec90337e26e78de5be6b3e8284977156e62c4
Louis Chavey <louis@chavey.org>
2025-04-27 15:54:13 -0500

    milestone 4 - PD

--- plot.py
+++ plot.py
@@ -9,12 +9,11 @@ def main():
     df = df.dropna()
     # breakpoint()
     pitchx10 = np.array(df.iloc[:,0]) * 10
-    desired_pitchx10 = np.array(df.iloc[:,1])
     # thrust = np.array(df.iloc[:,2])
-    motor_1 = np.array(df.iloc[:,2])
-    motor_2 = np.array(df.iloc[:,3])
-    motor_3 = np.array(df.iloc[:,4])
-    motor_4 = np.array(df.iloc[:,5])
+    motor_1 = np.array(df.iloc[:,1])
+    motor_2 = np.array(df.iloc[:,2])
+    motor_3 = np.array(df.iloc[:,3])
+    motor_4 = np.array(df.iloc[:,4])
 
     t = np.linspace(0, len(pitchx10)-1, len(pitchx10))
 
@@ -22,13 +21,12 @@ def main():
 
     plt.figure(figsize=(10, 5))
     plt.plot(t, pitchx10, color='blue', label=f'pitch_filter')
-    plt.plot(t, desired_pitchx10, color='orange', label=f'gyro')
     plt.plot(t, motor_1, color='green', label=f'motor_1')
     plt.plot(t, motor_2, color='cyan', label=f'motor_2')
     plt.plot(t, motor_3, color='red', label=f'motor_3')
     plt.plot(t, motor_4, color='yellow', label=f'motor_4')
     plt.legend()
-    plt.savefig(f'D_motor_test.png')
+    plt.savefig(f'PD_motor_test.png')
 
     return
 
