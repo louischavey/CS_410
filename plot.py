8e7ddaf084bb886957f9026c8f75dab784b70cda
Louis Chavey <louis@chavey.org>
2025-04-27 16:31:06 -0500

    milestone 5

--- plot.py
+++ plot.py
@@ -8,25 +8,26 @@ def main():
     df = pd.read_csv(filename)
     df = df.dropna()
     # breakpoint()
-    pitchx10 = np.array(df.iloc[:,0]) * 10
-    # thrust = np.array(df.iloc[:,2])
-    motor_1 = np.array(df.iloc[:,1])
-    motor_2 = np.array(df.iloc[:,2])
-    motor_3 = np.array(df.iloc[:,3])
-    motor_4 = np.array(df.iloc[:,4])
+    pitch_accel = np.array(df.iloc[:,0]) * 10
+    pitch_filter = np.array(df.iloc[:,1]) * 10
+    motor_1 = np.array(df.iloc[:,2]) / 10
+    motor_2 = np.array(df.iloc[:,3]) / 10
+    motor_3 = np.array(df.iloc[:,4]) / 10
+    motor_4 = np.array(df.iloc[:,5]) / 10
 
-    t = np.linspace(0, len(pitchx10)-1, len(pitchx10))
+    t = np.linspace(0, len(pitch_accel)-1, len(pitch_accel))
 
     
 
     plt.figure(figsize=(10, 5))
-    plt.plot(t, pitchx10, color='blue', label=f'pitch_filter')
+    plt.plot(t, pitch_accel, color='blue', label=f'pitch_accel')
+    plt.plot(t, pitch_filter, color='red', label=f'pitch_filter')
     plt.plot(t, motor_1, color='green', label=f'motor_1')
     plt.plot(t, motor_2, color='cyan', label=f'motor_2')
-    plt.plot(t, motor_3, color='red', label=f'motor_3')
+    plt.plot(t, motor_3, color='purple', label=f'motor_3')
     plt.plot(t, motor_4, color='yellow', label=f'motor_4')
     plt.legend()
-    plt.savefig(f'PD_motor_test.png')
+    plt.savefig(f'DLPF_normal.png')
 
     return
 
