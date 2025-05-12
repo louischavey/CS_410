5b10d5a7893ed1e51729a140e34984b1642e8f95
Louis Chavey <louis@chavey.org>
2025-05-11 20:03:28 -0500

    hovering tenatively done

--- plot.py
+++ plot.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import time
 
 def main():
     axis = 'pitch'
@@ -8,31 +9,26 @@ def main():
     df = pd.read_csv(filename)
     df = df.dropna()
     # breakpoint()
-    roll_desired = np.array(df.iloc[:,0]) * 10
-    roll_filter = np.array(df.iloc[:,1]) * 10
-    pitch_desired = np.array(df.iloc[:,2]) * 10
-    pitch_filter = np.array(df.iloc[:,3]) * 10
-    motor_1 = np.array(df.iloc[:,4]) / 10
-    motor_2 = np.array(df.iloc[:,5]) / 10
-    motor_3 = np.array(df.iloc[:,6]) / 10
-    motor_4 = np.array(df.iloc[:,7]) / 10
-    thrust = np.array(df.iloc[:,8]) / 10
+    pitch_desired = np.array(df.iloc[:,0])
+    pitch_filter = np.array(df.iloc[:,1])
+    roll_desired = np.array(df.iloc[:,2])
+    roll_filter = np.array(df.iloc[:,3])
+    yaw_desired = np.array(df.iloc[:,4])
+    yaw_raw = np.array(df.iloc[:,5])
 
     t = np.linspace(0, len(roll_desired)-1, len(roll_desired))
+
     
 
     plt.figure(figsize=(10, 5))
     plt.plot(t, roll_desired, color='blue', label=f'roll_desired')
     plt.plot(t, roll_filter, color='red', label=f'roll_filter')
-    plt.plot(t, pitch_desired, color='orange', label=f'pitch_desired')
-    plt.plot(t, pitch_filter, color='black', label=f'pitch_filter')
-    plt.plot(t, motor_1, color='green', label=f'front_left')
-    plt.plot(t, motor_2, color='cyan', label=f'back_left')
-    plt.plot(t, motor_3, color='purple', label=f'front_right')
-    plt.plot(t, motor_4, color='yellow', label=f'back_right')
-    plt.plot(t, thrust, color='brown', label=f'thrust')
+    plt.plot(t, motor_1, color='green', label=f'motor_1')
+    plt.plot(t, motor_2, color='cyan', label=f'motor_2')
+    plt.plot(t, motor_3, color='purple', label=f'motor_3')
+    plt.plot(t, motor_4, color='yellow', label=f'motor_4')
     plt.legend()
-    plt.savefig(f'No_Rig_Test.png')
+    plt.savefig(f'Roll_PID.png')
 
     return
 
